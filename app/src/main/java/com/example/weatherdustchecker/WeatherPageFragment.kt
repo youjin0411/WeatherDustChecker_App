cfed10de531a9e8ac04d89252760fc2c63da6648
Kim Yu Jin <w2117@e-mirin.hs.kr>
2022-07-26 15:28:42 +0900

    Wheather App

---
+++ app/src/main/java/com/example/weatherdustchecker/WeatherPageFragment.kt
@@ -0,0 +1,21 @@
+package com.example.weatherdustchecker
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class WeatherPageFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater
+            .inflate(R.layout.weater_page_fragment,
+            container,false)
+
+        return view
+    }
+}
