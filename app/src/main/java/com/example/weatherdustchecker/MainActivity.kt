e0677015c1fc89d148491d355dfafd75478b6fad
Kim Yu Jin <w2117@e-mirin.hs.kr>
2022-07-27 10:26:39 +0900

    Wheather App 날씨 API 활용 전

--- app/src/main/java/com/example/weatherdustchecker/MainActivity.kt
+++ app/src/main/java/com/example/weatherdustchecker/MainActivity.kt
@@ -7,5 +7,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //상단 앱 제목 표시줄 숨기기
+        supportActionBar?.hide()
+
+        //TODO : WeatherPageFragment 표시하기 (FrameLayout에 넣어주기)
+        val transaction = supportFragmentManager.beginTransaction()
+        //TODO : newInstance 클래스 메서드 정의해서 status값(문자열),
+        // temperature 값(DOuble) 전달할 수 있도록 해 주기
+        // 해당 값은 모두 프래그먼트의 번들 객체에 저장되어야 함
+        transaction.add(R.id.fragment_container,WeatherPageFragment.newInstance("화창", 10.0))
+        transaction.commit()
     }
 }
--- app/src/main/java/com/example/weatherdustchecker/WeatherPageFragment.kt
+++ app/src/main/java/com/example/weatherdustchecker/WeatherPageFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class WeatherPageFragment : Fragment() {
@@ -16,6 +19,33 @@ class WeatherPageFragment : Fragment() {
             .inflate(R.layout.weater_page_fragment,
             container,false)
 
+        //TODO: arguments 값 참조해서 두 개 값 가져오고, 해당하는 뷰에
+        // 출력하기
+        val statusText = view.findViewById<TextView>(R.id.weather_status_text)
+        val temperatureText = view.findViewById<TextView>(R.id.weather_temp_text)
+        val weatherImage = view.findViewById<ImageView>(R.id.weather_icon)
+
+        statusText.text = arguments?.getString("status")
+        temperatureText.text = arguments?.getDouble("temperature").toString()
+
+        //TODO : ImageView 가져와서 sum 이미지 출력하기
+        weatherImage.setImageResource(arguments?.getInt("res_id")!!)
         return view
+    }
+
+    companion object{
+        fun newInstance(status: String, temperature:Double)
+        :WeatherPageFragment {
+            val fragment = WeatherPageFragment()
+
+            //번들 객체에 필요한 정보를 저장
+            val args = Bundle()
+            args.putString("status", status)
+            args.putDouble("temperature",temperature)
+            args.putInt("res_id", R.drawable.sun)
+            fragment.arguments = args
+
+            return fragment
+        }
     }
 }
